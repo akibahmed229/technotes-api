c5eeb5bc3b4d8f511971573fae46d081afb900b2
akibahmed229 <akib4418@gmail.com>
2023-05-16 21:29:43 +0600

    Ready to deploy Backend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 // sites that are allowed to request the API
 const allowedOrigins = [
-    "https://technotes-dg5l.onrender.com",
+  "https://technotes-dg5l.onrender.com",
+  "https://technotess.netlify.app",
 ];
 //   "http://localhost:5173",
 //   "http://localhost:3000",
